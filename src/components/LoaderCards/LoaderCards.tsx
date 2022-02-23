a1fd433cf1b9fab0ec2d0f452f8cbaf36137e3a8
c4chris124 <c4chris124@gmail.com>
2022-02-22 23:07:31 -0600

    Adding loader files

---
+++ src/components/LoaderCards/LoaderCards.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const LoaderCards = () => (
+  <ContentLoader 
+    speed={2}
+    width={286}
+    height={175}
+    viewBox="0 0 286 175"
+    backgroundColor="#6a4dbc"
+    foregroundColor="#8973c4"
+  >
+    <rect x="0" y="0" rx="16" ry="16" width="285" height="174" />
+  </ContentLoader>
+)
+
+export default LoaderCards
