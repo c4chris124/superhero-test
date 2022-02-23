e242475c2d8f89cfe8af35f6c2601aa6c1329cf3
c4chris124 <c4chris124@gmail.com>
2022-02-22 23:19:27 -0600

    Adding basic states and structures

--- src/components/SuperHeros/SuperHeros.tsx
+++ src/components/SuperHeros/SuperHeros.tsx
@@ -1,8 +1,36 @@
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { FixedSizeList } from "react-window";
+import { getHeros } from "../../state/actions/actionCreators";
+import { RootStore } from '../../state/store/store';
+import HeroItem from "../HeroItem/HeroItem";
+import LoaderFirstCard from "../LoaderFirstCard/LoaderFirstCard";
+import SearchBar from "../SearchBar/SearchBar";
+import st from './SuperHeros.module.css'
+
 
 const SuperHeros = () => {
-  return (
-    <div>SuperHeros</div>
-  )
+    const dispatch = useDispatch()
+    const herostate = useSelector((state: RootStore) => state.heros.heros)
+    const loading = useSelector((state: RootStore) => state.heros.loading)
+    const Row = useCallback(() => {}, [])
+
+    useEffect(() => {
+        dispatch(getHeros())
+    }, [dispatch])
+
+    console.log(herostate);
+
+
+    return (
+        <div>
+            <div>
+                <h1>All Superheros</h1>
+                <SearchBar />
+            </div>
+            
+        </div>
+    )
 }
 
 export default SuperHeros
