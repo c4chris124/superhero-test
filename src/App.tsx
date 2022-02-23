d98623d0993cb5e1db43523f27842ba0f8e84adf
c4chris124 <c4chris124@gmail.com>
2022-02-23 15:44:38 -0600

    Adding some styles and bringing back useEffect to load games

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import LikedHeros from './components/LikedHeros/LikedHeros';
 import NavBar from './components/NavBar/NavBar';
 import SuperHeros from './components/SuperHeros/SuperHeros';
+import { getHeros } from './state/actions/actionCreators';
 
 function App() {
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getHeros())
+}, [dispatch])
 
   return (
     <div className="App">
