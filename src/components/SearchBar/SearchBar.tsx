6ed25b659d27ebab6c0eef81afefc90ce0c3ec07
c4chris124 <c4chris124@gmail.com>
2022-02-24 13:17:54 -0600

    Adding styles search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getHeros } from '../../state/actions/actionCreators'
+import st from './SearchBar.module.css'
+import searchIcon from '../../assets/search/search.svg'
+import cancel from '../../assets/cancel/cancel.svg'
+
 
 const SearchBar = () => {
-    const dispatch = useDispatch()
-    const[heroName, setHeroName] = useState("")
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setHeroName(e.target.value)
-    const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        dispatch(getHeros)
-    }
+  const dispatch = useDispatch()
+  const [heroName, setHeroName] = useState("")
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setHeroName(e.target.value)
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch(getHeros)
+  }
   return (
-    <div>
-    <input type="text" onChange={handleChange} />
-    <button>Search</button>
+    <div className={st.container}>
+      <form action="">
+        <div className={st.search_input_container}>
+          <img className={st.search_icon} src={searchIcon} alt="" />
+          <input className={st.search_input} type="text" onChange={handleChange} />
+          <img className={st.search_icon} src={cancel} alt="" />
+
+        </div>
+      </form>
     </div>
   )
 }
