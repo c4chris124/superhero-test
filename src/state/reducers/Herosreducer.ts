7dbfbcf70bd93ee330b1d4103f0dcaf31a18cde8
c4chris124 <c4chris124@gmail.com>
2022-02-27 08:32:54 -0600

    reducer for remove liked items

--- src/state/reducers/Herosreducer.ts
+++ src/state/reducers/Herosreducer.ts
@@ -1,4 +1,4 @@
-import { HeroDispatchTypes, HEROS_SUCCESS, LOADING_HEROS, HEROS_FAIL, HeroType, GET_LIKED_HEROS, ADD_LIKED_HERO } from "../actions/actionTypes";
+import { HeroDispatchTypes, HEROS_SUCCESS, LOADING_HEROS, HEROS_FAIL, HeroType, GET_LIKED_HEROS, ADD_LIKED_HERO, REMOVED_LIKED_HEROS } from "../actions/actionTypes";
 
 
 export interface defaultStateI {
@@ -41,6 +41,16 @@ const Herosreducer = (state: defaultStateI = defaultState, action: HeroDispatchT
             return {
                 loading: false,
             }
+            case REMOVED_LIKED_HEROS:
+                for (let i = 0; i < state.likedHeros.length; i++) {
+                    if (state.likedHeros[i] === action.payload) {
+                        state.likedHeros.splice(i,1)
+                    }
+                  }
+                return {
+                    loading: false,
+                    likedHeros: action.payload
+                }    
 
         default:
             return state
