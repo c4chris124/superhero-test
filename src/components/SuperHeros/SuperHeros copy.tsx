5ab0448c4b46b94e9396eadef0970c9a6d97daf4
c4chris124 <c4chris124@gmail.com>
2022-03-29 14:09:56 -0600

    fixing more warnings

--- src/components/SuperHeros/SuperHeros copy.tsx
+++ src/components/SuperHeros/SuperHeros copy.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState }  from "react";
+import { useEffect, useState }  from "react";
 import { useSelector } from "react-redux"
 import { RootStore } from '../../state/store/store';
 import LoaderCardsContainer from "../LoaderCards/LoaderCardsContainer";
